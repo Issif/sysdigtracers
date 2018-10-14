da98ce1c636894cab4e7584120f5595cd630479b
Thomas Labarussias <thomas.labarussias@fr.clara.net>
2018-10-14 20:33:13 +0200

    add comment for godoc + more exported fields

--- sysdigtracers.go
+++ sysdigtracers.go
@@ -8,11 +8,9 @@ import (
 
 // Tracer represents a tracer
 type Tracer struct {
-	// (see: https://github.com/draios/sysdig/wiki/Tracers#fields-explanation)
-
-	id   string // id string should be a 64bit integer or t, p, pp.
-	tags string // tag string should be a list of one or more strings separated by periods. if empty, name of caller function is set.
-	args string // args string is a list of key-value pairs to be associated with the tracer (optionnal).
+	Id   string // Id string should be a 64bit integer or t, p, pp (see: https://github.com/draios/sysdig/wiki/Tracers#fields-explanation).
+	Tags string // Tags string should be a list of one or more strings separated by periods. if empty, name of caller function is set.
+	Args string // Args string is a list of key-value pairs to be associated with the tracer (optionnal).
 }
 
 // getFunctionName retrieves name of caller function.
@@ -24,32 +22,33 @@ func getFunctionName() string {
 }
 
 // Entry emits an entry event in /dev/null and returns a tracer struct.
-// id, tags and args can be empty string.
-// if tags is empty, name of caller function is set.
-func Entry(id string, element ...string) Tracer {
-	if element[0] == "" {
-		element[0] = getFunctionName()
+// id and elements can be empty strings.
+// first string in elements will be used as tags, other strings will be arguments.
+// if elements is empty (no tag), name of caller function is set.
+func Entry(id string, elements ...string) Tracer {
+	if elements[0] == "" {
+		elements[0] = getFunctionName()
 	}
 
 	var args string
-	if len(element) > 1 {
-		args = element[1]
+	if len(elements) > 1 {
+		args = elements[1]
 	}
 
-	t := Tracer{id: id, tags: element[0], args: args}
+	t := Tracer{Id: id, Tags: elements[0], Args: args}
 
-	d := []byte(">:" + t.id + ":" + t.tags + ":" + t.args + ":")
+	d := []byte(">:" + t.Id + ":" + t.Tags + ":" + t.Args + ":")
 	ioutil.WriteFile("/dev/null", d, 0777)
 	return t
 }
 
 // Exit emits an exit event in /dev/null.
-// args can be empty, if not, args of tracer are replaced.
+// args can be empty, if not, args of tracer are overrided by it.
 func (t Tracer) Exit(args ...string) {
 	if len(args) != 0 {
-		t.args = args[0]
+		t.Args = args[0]
 	}
 
-	d := []byte("<:" + t.id + ":" + t.tags + ":" + t.args + ":")
+	d := []byte("<:" + t.Id + ":" + t.Tags + ":" + t.Args + ":")
 	ioutil.WriteFile("/dev/null", d, 0777)
 }
